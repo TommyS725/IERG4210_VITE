ed347d9f4d5b064a7e8df457828bcc577f2e0e6c
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2024-03-03 20:24:32 +0800

    fix dc

--- frontend/src/utils/debounce.ts
+++ frontend/src/utils/debounce.ts
@@ -2,7 +2,7 @@
 
 
 export default function debounce<F extends (...args: any[]) => any>(func: F, delay: number): (...args: Parameters<F>) => void {
-    let debounceTimer:Timer;
+    let debounceTimer:Parameters<typeof clearTimeout>[number];
     return (...args: Parameters<F>) => {
       clearTimeout(debounceTimer);
       debounceTimer = setTimeout(() => func(...args), delay);
